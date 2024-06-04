eeb16d3d5703c8fd351a0964ae55215eb40fab60
kitare17 <hoctapak@gmail.com>
2024-06-04 15:51:07 +0700

    Music vip pro

---
+++ app/src/main/java/com/example/labmusic/MusicController.java
@@ -0,0 +1,11 @@
+package com.example.labmusic;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context context) {
+        super (context);
+    }}
+
